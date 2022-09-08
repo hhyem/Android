c7966fe5a358190543e3543566d34100a9931dbc
hyemin <88538911+hhyem@users.noreply.github.com>
2022-09-08 18:36:20 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,64 @@
-package com.cookandroid.helloandroid;
+package com.cookandroid.baseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<Internet> extends AppCompatActivity {
+
+    Button button1, button2, button3, button4;
+    Intent mlntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setBackgroundColor(Color.GRAY);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setBackgroundColor(Color.GREEN);
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setBackgroundColor(Color.RED);
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setBackgroundColor(Color.YELLOW);
+        button1.setTextColor(Color.BLACK);
+        button2.setTextColor(Color.BLACK);
+        button3.setTextColor(Color.BLACK);
+        button4.setTextColor(Color.BLACK);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mlntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mlntent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mlntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mlntent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mlntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mlntent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
